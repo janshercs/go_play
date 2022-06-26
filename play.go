fb2ba9301111d688f7ce5ee8b7aa9561c4c7e181
jansen her <jansen.her@foodpanda.com>
2022-06-26 22:22:00 +0800

    Add testcase file.

--- play.go
+++ play.go
@@ -1,5 +1,60 @@
 package goplay
 
-func getStringsThatMeetRules(rules, strings []string) []string {
-	return []string{""}
+import "strings"
+
+func bruteForceBinsRules(rules, bins []string) []string {
+	subset := make([]string, 0, len(bins))
+	for _, bin := range bins {
+		for _, rule := range rules {
+			if strings.HasPrefix(bin, rule) {
+				subset = append(subset, bin)
+			}
+		}
+	}
+	return subset
+}
+
+var (
+	mapSet  = false
+	ruleSet = map[string]bool{}
+)
+
+// technically an O(n) + O(m) operation
+func mapBinsRules(rules, bins []string) []string {
+	subset := make([]string, 0, len(bins))
+	initRules(rules)
+	for _, bin := range bins {
+		if fulfilsRules(bin) {
+			subset = append(subset, bin)
+		}
+	}
+	return subset
+}
+
+// O(m) operation
+func initRules(rules []string) {
+	if mapSet {
+		return
+	}
+	for _, rule := range rules {
+		ruleSet[rule] = true
+	}
+	mapSet = true
+}
+
+// Technically an O(1) operation
+func fulfilsRules(bin string) bool {
+	max := 6
+	if len(bin) < 6 {
+		max = len(bin) // setting length to be min(len(bin), 6)
+	}
+
+	var ok bool
+	for i := 2; i < max+1; i++ { // O(5)
+		if ok = ruleSet[bin[:i]]; !ok { // O(1)
+			continue
+		}
+		return true
+	}
+	return false
 }
--- play_test.go
+++ play_test.go
@@ -6,6 +6,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStringRules(t *testing.T) {
-	assert.Equal(t, []string{"a"}, getStringsThatMeetRules([]string{""}, []string{""}))
+var (
+	answers = []string{
+		"452626", "159452", "52450", "452778", "556208",
+	}
+)
+
+func TestBruteforce(t *testing.T) {
+	assert.Equal(t, answers, bruteForceBinsRules(rules, testcase))
+}
+
+func TestMapAlgo(t *testing.T) {
+	assert.Equal(t, answers, mapBinsRules(rules, testcase))
+}
+
+// cpu: Intel(R) Core(TM) i5-10500 CPU @ 3.10GHz
+// BenchmarkBruteforce-12           5484798               213.9 ns/op
+// PASS
+// ok      go-play 1.857s
+
+func BenchmarkBruteforce(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		bruteForceBinsRules(rules, testcase)
+	}
+}
+
+func BenchmarkMapAlgo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mapBinsRules(rules, testcase)
+	}
 }
